97d8b6d30515119d1c68d3ea973ba54d15a2d416
FakirRabih <fakirrabih@gmail.com>
2024-02-02 22:01:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const client = new Client({
     clientId: "YOUR_CLIENT_ID",
   }),
 });
-await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+
 
 client.on("qr", (qr) => {
   console.log("QR RECEIVED", qr);
